1868cd045f9d2b0b24e39a9dab85a54b192307e9
Nguyễn Lê Thiện Ân <thienan092@gmail.com>
2019-06-27 17:58:07 +0700

    Added Private Machine Learning with PATE project
    Train a DP model using PATE method.

---
+++ Private Machine Learning with PATE/utils.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def cal_max(teacher_preds, num_labels):
+    indices = []
+    for i in range(teacher_preds.shape[1]):
+        label_counts = np.bincount(teacher_preds[:,i], minlength=num_labels)
+        max_label = np.argmax(label_counts)
+        indices.append(max_label)
+    return np.array(indices)
+
+def noisy_max(teacher_preds, privacy_loss_lv, n_labels): 
+    indices = []
+    for i in range(teacher_preds.shape[1]):
+        label_counts = np.bincount(teacher_preds[:,i], minlength=n_labels)
+        noisy_count = label_counts + np.random.laplace(np.zeros(len(label_counts)), np.ones(len(label_counts))/privacy_loss_lv, len(label_counts))
+        indices.append(np.argmax(label_counts))
+    return np.array(indices)
