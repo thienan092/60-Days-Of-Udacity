4c898e1ae374b8e53f1a92612a1a5b8928856123
Nguyễn Lê Thiện Ân <thienan092@gmail.com>
2019-07-18 09:39:01 +0700

    Corrected utils.py
    Corrected the noisy_max function.

--- Private Machine Learning with PATE/utils.py
+++ Private Machine Learning with PATE/utils.py
@@ -12,6 +12,6 @@ def noisy_max(teacher_preds, privacy_loss_lv, n_labels):
     indices = []
     for i in range(teacher_preds.shape[1]):
         label_counts = np.bincount(teacher_preds[:,i], minlength=n_labels)
-        noisy_count = label_counts + np.random.laplace(np.zeros(len(label_counts)), np.ones(len(label_counts))/privacy_loss_lv, len(label_counts))
-        indices.append(np.argmax(label_counts))
+        noisy_counts = label_counts + np.random.laplace(np.zeros(len(label_counts)), np.ones(len(label_counts))/privacy_loss_lv, len(label_counts))
+        indices.append(np.argmax(noisy_counts))
     return np.array(indices)
